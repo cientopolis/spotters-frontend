aad845c0bf28d2ea605de6698d8b8d56254a985b
Jorge Agustín Marisi <agustinmarisi@gmail.com>
2016-11-18 18:53:33 -0300

    #11 ordeno clasificaciones

--- src/components/candidateCard/classificationList.component.ts
+++ src/components/candidateCard/classificationList.component.ts
@@ -1,15 +1,30 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Candidate } from '../../models/candidate';
 import { Classification } from '../../models/classification';
 import { Workflow } from '../../models/workflow';
+import _ from 'lodash';
 
 @Component({
   selector: 'classification-list',
   templateUrl: 'classification-list.html'
 })
-export class ClassificationListComponent {
+export class ClassificationListComponent implements OnInit {
   @Input() candidate: Candidate = null;
   @Input() classifications: Classification[] = [];
   @Input() workflow: Workflow = null;
   @Input() expert: boolean = false;
+
+  calculatePositiveVotes(classification: Classification): number {
+    return _.filter(classification.votes, { 'positive': true }).length;
+  }
+
+  calculateNegativeVotes(classification: Classification): number {
+    return _.filter(classification.votes, { 'positive': false }).length;
+  }
+
+  ngOnInit() {
+    this.classifications = _.orderBy(this.classifications, (classification) => {
+      return this.calculatePositiveVotes(classification) - this.calculateNegativeVotes(classification);
+    }, ['desc']);
+  }
 }
